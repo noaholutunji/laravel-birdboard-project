d0e5e41260f2af7e79c188b9a79654166942aebd
Noah Osuolale <osuolale49@gmail.com>
2021-06-21 08:40:26 +0100

    code cleanup

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-sm-right mb-2 block">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-sm-right block mb-1">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="border w-full form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-sm-right mb-2 block">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-sm-right mt-4 mb-1 block">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="border w-full form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -40,7 +40,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-sm-right mb-2 block">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-sm-right mt-4 mb-1 block">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="border w-full form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -54,7 +54,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-sm-right mb-2 block">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-sm-right mt-4 mb-1 block">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="border w-full mb-2 form-control" name="password_confirmation" required autocomplete="new-password">
@@ -63,7 +63,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="button is-link mr-2">
+                                <button type="submit" class="button is-link mr-2 mt-2">
                                     {{ __('Register') }}
                                 </button>
                             </div>
